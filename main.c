b5f1dae9b2ffb7bd191d63aaa11d2c3a15d9ad54
ie702272 <ie702272@iteso.mx>
2018-03-11 13:50:38 -0600

    UART_implementado

--- main.c
+++ main.c
@@ -48,6 +48,29 @@
 #include "event_groups.h"
 #include "queue.h"
 
+/* UART instance and clock */
+#define DEMO_UART UART0
+#define DEMO_UART_CLKSRC UART0_CLK_SRC
+#define DEMO_UART_CLK_FREQ CLOCK_GetFreq(UART0_CLK_SRC)
+#define ECHO_BUFFER_LENGTH 8
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/* UART user callback */
+void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData);
+char* concat(const char *s1, const char *s2);
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+uart_handle_t g_uartHandle;
+uint8_t g_txBuffer[ECHO_BUFFER_LENGTH] = {0};
+uint8_t g_rxBuffer[ECHO_BUFFER_LENGTH] = {0};
+volatile bool rxBufferEmpty = true;
+volatile bool txBufferFull = false;
+volatile bool txOnGoing = false;
+volatile bool rxOnGoing = false;
 
 /* Counters limit */
 #define SECONDS_LIMIT 60
@@ -171,6 +194,7 @@ void hours_task (void *arg)
 
 void print_task (void *arg)
 {
+	uart_transfer_t xfer;
     time_msg_t algoRead;
     uint8_t segundos = 0;
     uint8_t minutos = 0;
@@ -212,12 +236,21 @@ void print_task (void *arg)
             }
         }
 
-        PRINTF ("\033[1A");
-        PRINTF("\033[18D");
-        PRINTF("\033[2J");
-        PRINTF("   %d",horas);
-        PRINTF(":%d",minutos);
-        PRINTF(":%d\n",segundos);
+        char* horas_minutos = concat((char*)horas, (char*)minutos);
+        char* tiempo = concat(horas_minutos, (char*)segundos);
+
+        /* Send g_tipString out. */
+         xfer.data = (uint8_t*)tiempo;
+         xfer.dataSize = sizeof(tiempo) - 1;
+         txOnGoing = true;
+         UART_TransferSendNonBlocking(DEMO_UART, &g_uartHandle, &xfer);
+
+//        PRINTF ("\033[1A");
+//        PRINTF("\033[18D");
+//        PRINTF("\033[2J");
+//        PRINTF("   %d",horas);
+//        PRINTF(":%d",minutos);
+//        PRINTF(":%d\n",segundos);
 
 
     }
@@ -276,6 +309,15 @@ int main (void)
     xTaskCreate (alarm_task, "Alarma", 300, NULL,
     configMAX_PRIORITIES - 1, NULL);
 
+    uart_config_t config;
+    UART_GetDefaultConfig(&config);
+    config.baudRate_Bps = BOARD_DEBUG_UART_BAUDRATE;
+    config.enableTx = true;
+    config.enableRx = true;
+
+    UART_Init(DEMO_UART, &config, DEMO_UART_CLK_FREQ);
+    UART_TransferCreateHandle(DEMO_UART, &g_uartHandle, UART_UserCallback, NULL);
+
     vTaskStartScheduler ();
 
     while (1)
@@ -285,3 +327,30 @@ int main (void)
     return 0;
 }
 
+
+extern void UART0_DriverIRQHandler(void);
+
+/* UART user callback */
+void UART_UserCallback(UART_Type *base, uart_handle_t *handle, status_t status, void *userData)
+{
+    userData = userData;
+
+    if (kStatus_UART_TxIdle == status)
+    {
+        txBufferFull = false;
+        txOnGoing = false;
+    }
+
+    if (kStatus_UART_RxIdle == status)
+    {
+        rxBufferEmpty = false;
+        rxOnGoing = false;
+    }
+}
+
+char* concat(const char *s1, const char *s2){
+	 char *result = malloc(strlen(s1)+strlen(s2)+1);
+	 strcpy(result, s1);
+	 strcat(result, s2);
+	 return result;
+}
